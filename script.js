9c3c66f0fe198ee315ce9328e61d2c0460054985
Muhammad Abdullah <abdullahferoz313@gmail.com>
2024-06-16 13:15:21 +0500

    Added a variable 'photoCount' to control the number of images

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-let activeLink = "";
 const display = document.getElementsByClassName('display')[0];
 const scrollSection = document.getElementsByClassName('scroll-section')[0];
+const photoCount = 20;
 
-for(let i = 0 ; i < 10 ; i++){
+for(let i = 0 ; i < photoCount ; i++){
     let imgBox = `
-            <div class="img-box">
+    <div class="img-box">
                 <img src="https://picsum.photos/700/500?random=${i}">
-            </div>`
+                </div>`
     scrollSection.innerHTML = scrollSection.innerHTML + imgBox
 }
 
@@ -16,12 +16,13 @@ const resetClick = ()=>{
     })
 }
 
+
 Array.from(document.getElementsByClassName("img-box")).forEach((e, i) => {
     e.addEventListener("click", () =>{
         resetClick();
         if(!e.classList.contains('selected')){
             e.classList.toggle("selected");
-            activeLink = e.querySelector("img").src
+            let activeLink = e.querySelector("img").src
             let activeImg = `
             <img src="${activeLink}" alt= "activeImage">
             `
